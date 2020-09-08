228abe98eb01eddbfeffb9b1a34a84d45424acba
Yassine <yassin23raki@gmail.com>
2020-09-08 11:13:50 +0100

    base javascript api setup

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = "lnwXimsyqIWXlJr_6tx3lCJmzp0lnsSX-iChfgjo1Vc"
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }    
+}
+
+// On Load
+getPhotos();
